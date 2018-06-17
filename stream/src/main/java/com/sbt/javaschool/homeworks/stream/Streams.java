c6d2b41297c0ee32e09d29d21f45e8b8054795f4
seet61 <seet_seet@mail.ru>
2018-06-17 21:11:42 +0300

    add variant with stream api

--- stream/src/main/java/com/sbt/javaschool/homeworks/stream/Streams.java
+++ stream/src/main/java/com/sbt/javaschool/homeworks/stream/Streams.java
@@ -1,37 +1,67 @@
 package com.sbt.javaschool.homeworks.stream;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Streams<T> {
-    private T t;
+    private List<T> list;
 
-    private Streams(T t) {
-        this.t = t;
+    /**
+     * Конструктор класса
+     * @param list коллекция передаваемая констурктору
+     */
+    private Streams(List<? extends T> list) {
+        this.list = (List<T>) list;
     }
 
-
-    public static <T> Streams<T> of(T t) {
-        return new Streams<T>(t);
+    /**
+     * Создаем Streams для обработки
+     * @param list типа данных
+     * @param <T> тип данных
+     * @return новый объект класса Streams
+     */
+    public static <T> Streams<T> of(List<T> list) {
+        return new Streams<T>(list);
     }
 
-    public <T> Streams<T> filter(Predicate<T> predicate) {
-        if (predicate.test((T) t)) {
-            return new Streams<T>((T) t);
+    /**
+     * Оставляет в коллекции только те элементы, которые удовлетворяют условию в лямбде.
+     * @param predicate условие проверки
+     * @return this.list
+     */
+    public Streams<T> filter(Predicate<T> predicate) {
+        for (Iterator<T> it = list.iterator(); it.hasNext(); ) {
+            T element = it.next();
+            if (!predicate.test(element)) {
+                it.remove();
+            }
         }
 
-        return null;
+        return this;
     }
 
-    public <T> Streams<T> transform(T t) {
-        return new Streams<T>((T) t);
+    /**
+     * преобразует элемент в другой.
+     * @param function
+     * @return
+     */
+    public Streams<T> transform(Function<? super T, ? extends T> function) {
+        List<T> newList = new ArrayList<>();
+        for (Iterator<T> itl = list.iterator(); itl.hasNext(); ) {
+            T newItem = function.apply(itl.next());
+            newList.add(newItem);
+        }
+        return new Streams<>(newList);
     }
 
+    /*
     public HashMap toMap(Predicate<T> p1, Predicate<T> p2) {
         if (p1.test(t)) {
-            return new HashMap(t,t.toString());
+            return new HashMap(t.toString());
         }
 
         return null;
     }
+    */
 }
