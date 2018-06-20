6f4d7eb50a5a73a83d6f267c980680cbdcbe0b86
seet61 <seet_seet@mail.ru>
2018-06-20 16:41:29 +0300

    add ThreadPool interface

---
+++ thread-pool/src/main/java/com/sbt/javaschool/homeworks/threadpool/ThreadPool.java
@@ -0,0 +1,14 @@
+package com.sbt.javaschool.homeworks.threadpool;
+
+public interface ThreadPool {
+    /**
+     * запускает потоки. Потоки бездействуют, до тех пор пока не появится новое задание в очереди (см. execute)
+     */
+    void start();
+
+    /**
+     * складывает это задание в очередь. Освободившийся поток должен выполнить это задание. Каждое задание должны быть выполнено ровно 1 раз
+     * @param runnable
+     */
+    void execute(Runnable runnable);
+}
