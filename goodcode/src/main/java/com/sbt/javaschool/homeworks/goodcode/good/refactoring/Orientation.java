f0c96042ed67c7a79f44f38620482c0c156cd22f
seet61 <seet_seet@mail.ru>
2018-07-12 10:08:04 +0300

    add examples for refactoring and good variant for sal

---
+++ goodcode/src/main/java/com/sbt/javaschool/homeworks/goodcode/good/refactoring/Orientation.java
@@ -0,0 +1,7 @@
+package com.sbt.javaschool.homeworks.goodcode.good.refactoring;
+
+public enum Orientation {
+
+	NORTH, WEST, SOUTH, EAST;
+
+}
---
+++ goodcode/src/main/java/com/sbt/javaschool/homeworks/goodcode/good/refactoring/TractorInDitchException.java
@@ -0,0 +1,5 @@
+package com.sbt.javaschool.homeworks.goodcode.good.refactoring;
+
+public class TractorInDitchException extends RuntimeException {
+
+}
